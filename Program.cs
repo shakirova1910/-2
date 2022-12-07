35358e31d1317db53c5e21990a3834293d9ab8fd
Albina <shakirova.1910@mail.ru>
2022-12-07 17:06:59 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿int a;
+int b;
+
+Console.Write($"введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+
+Console.Write($"{a} + {b} = {a + b}\n");
+
+
+if (a > b)
+{ Console.WriteLine($"Число {a} больше чем {b}");
+}
+else if (a == b)
+{
+Console.WriteLine($"Числа {a} и {b} равны");
+}
+else
+{
+Console.WriteLine($"Число {b} больше чем {a}");
+}
