fc1d127664dc71c027a9dbd1ade7071fd5a76da6
shakirova1910 <119425273+shakirova1910@users.noreply.github.com>
2022-12-07 17:39:57 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿int a;
+## Задача 4 
+
+int a;
 int b;
 int c;
 int max;
@@ -21,4 +23,31 @@ Console.WriteLine($"{c}  = max");
 else if (c > a)
 {
 Console.WriteLine($"{a}  = max");
+}
+
+
+## Задача 2 
+
+int a;
+int b;
+
+Console.Write($"введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+
+Console.Write($"{a} + {b} = {a + b}\n");
+
+
+if (a > b)
+{ Console.WriteLine($"Число {a} больше чем {b}");
+}
+else if (a == b)
+{
+Console.WriteLine($"Числа {a} и {b} равны");
+}
+else
+{
+Console.WriteLine($"Число {b} больше чем {a}");
 }
